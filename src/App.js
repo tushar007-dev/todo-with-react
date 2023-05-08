3086566fd51309ab7721c5715eb047197c878312
tushari789 <tushar789imran@gmail.com>
2023-05-08 21:55:43 +0600

    todo project is done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  let [text, setText] = useState("");
+  let [error, setError] = useState(false);
+  let [taskArr, setTaskArr] = useState([]);
+  let [button, setButton] = useState(false);
+  let [editId, setSetEditId] = useState("");
+  let [editText, setSetEditText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleClick = () => {
+    if (text) {
+      taskArr.push(text);
+      let arr = [...taskArr];
+      setTaskArr(arr);
+      setText("");
+      console.log(taskArr);
+
+      if (error) {
+        setError(false);
+        console.log("render hoini");
+      }
+    } else {
+      console.log("nai");
+      setError(true);
+    }
+  };
+
+  let handleDelete = (id) => {
+    console.log(id);
+    taskArr.splice(id, 1);
+    let arr = [...taskArr];
+    setTaskArr(arr);
+    console.log(taskArr);
+  };
+
+  let handleCancle = () => {
+    setButton(false);
+    setText("");
+  };
+
+  let handleEdit = (text, id) => {
+    setButton(true);
+    setText(text);
+    console.log(text, id);
+
+    setSetEditId(id);
+    setSetEditText(text);
+  };
+
+  let handleUpdate = () => {
+    setButton(false);
+    setText("");
+    console.log(editId, editText);
+
+    taskArr[editId] = text;
+    console.log(taskArr);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <input onChange={handleChange} value={text} />
+      <button onClick={handleClick}>Add</button>
+
+      {button ? (
+        <>
+          <button onClick={handleUpdate}>Update</button>
+          <button onClick={handleCancle}>Calcle</button>
+        </>
+      ) : (
+        ""
+      )}
+
+      {error && <h1>write something</h1>}
+
+      <ol>
+        {taskArr.map((item, index) => (
+          <>
+            <li key={index}>
+              {item} - {index}
+              <button onClick={() => handleDelete(index)}>Delete</button>
+              <button onClick={() => handleEdit(item, index)}>Edit</button>
+            </li>
+          </>
+        ))}
+      </ol>
+    </section>
   );
 }
 
